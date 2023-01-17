86263ceaa61052326f5d11e7b5dafd67b79c1ee3
Mario St-Gelais <mariostg@beaubien.web-dns1.com>
2023-01-16 23:18:56 -0500

    Change stuff for production

---
+++ passenger_wsgi.py
@@ -0,0 +1,8 @@
+
+import os
+import sys
+from main.wsgi import application
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+environ = os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
