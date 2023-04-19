30e485e8b550b2a8739cf226f15ef9febcb10408
Mario St-Gelais <mario.stg@videotron.ca>
2023-04-18 23:31:10 -0400

    Redirect to site admin on login

--- users/views.py
+++ users/views.py
@@ -6,32 +6,35 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 from .forms import ProfileForm
 
+
 def user_login(request):
     if request.user.is_authenticated:
-        return redirect('profile')
+        return redirect("profile")
 
-    if request.method == 'POST':
-        username = request.POST['username'].lower()
-        password = request.POST['password']
+    if request.method == "POST":
+        username = request.POST["username"].lower()
+        password = request.POST["password"]
 
         try:
             user = User.objects.get(username=username)
         except:
-            messages.error(request, 'Username does not exist')
+            messages.error(request, "Username does not exist")
 
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
-            return redirect(request.GET['next'] if 'next' in request.GET else 'account')
+            return redirect(request.GET["next"] if "next" in request.GET else "site-admin")
         else:
-            messages.error(request, 'Username OR password is incorrect')
+            messages.error(request, "Username OR password is incorrect")
 
-    return render(request, 'users/login.html')
+    return render(request, "users/login.html")
+
 
 def user_logout(request):
     logout(request)
-    return redirect('login')
+    return redirect("login")
+
 
 def user_account(request):
     profile = request.user.profile
@@ -44,30 +47,34 @@ def user_account(request):
         context,
     )
 
+
 def edit_account(request):
     profile = request.user.profile
     form = ProfileForm(instance=profile)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
             form.save()
 
-            return redirect('account')
+            return redirect("account")
 
-    context = {'form': form}
-    return render(request, 'users/account_form.html', context)
+    context = {"form": form}
+    return render(request, "users/account_form.html", context)
+
 
 def user_profile(request, pk):
-    profile=UserProfile.objects.get(id=pk)
-    context={'user':profile}
-    return render(request, 'users/user_profile.html', context)
-    
+    profile = UserProfile.objects.get(id=pk)
+    context = {"user": profile}
+    return render(request, "users/user_profile.html", context)
+
+
 def user_account(request):
     profile = request.user.profile
-    context = {'profile': profile}
-    return render(request, 'users/account.html', context)
+    context = {"profile": profile}
+    return render(request, "users/account.html", context)
 
-@login_required(login_url='login')
+
+@login_required(login_url="login")
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    return render(request, "users/profiles.html")
