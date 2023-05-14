82faca1c0252f85600a5ea5eaeba2d0265516de4
Mario St-Gelais <mario.stg@videotron.ca>
2023-05-14 04:31:47 +0000

    Modify urlpatterns

--- main/urls.py
+++ main/urls.py
@@ -5,15 +5,16 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("guidedessites/siteadmin/", include("siteadmin.urls")),
-    path("guidedessites/users/", include("users.urls")),
-    path("guidedessites/", include("siteornitho.urls")),
-    path("guidedessites/habitat/", include("habitat.urls")),
-    path("guidedessites/soushabitat/", include("habitat.urls")),
-    path("guidedessites/geographie/", include("geographie.urls")),
+    path("siteadmin/", include("siteadmin.urls")),
+    path("users/", include("users.urls")),
+    path("", include("siteornitho.urls")),
+    path("habitat/", include("habitat.urls")),
+    path("soushabitat/", include("habitat.urls")),
+    path("geographie/", include("geographie.urls")),
     # path('__debug__/', include('debug_toolbar.urls')),
 ]
-# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
