bd6994b7e0b83791faac31d4c4a6693604149a97
Mario St-Gelais <mario.stg@videotron.ca>
2023-06-13 12:41:42 -0400

    Format longer lines

--- siteornitho/models.py
+++ siteornitho/models.py
@@ -33,51 +33,29 @@ class SiteOrnitho(models.Model):
     nom_du_site = models.CharField(max_length=125, verbose_name="Nom du site")
     # adresse
     # locid (hotspot)..
-    locid = models.CharField(
-        max_length=8, verbose_name="Id de localisation", null=True, blank=True
-    )
+    locid = models.CharField(max_length=8, verbose_name="Id de localisation", null=True, blank=True)
     # longitude..
-    longitude = models.DecimalField(
-        verbose_name="Longitude", max_digits=9, decimal_places=6
-    )
+    longitude = models.DecimalField(verbose_name="Longitude", max_digits=9, decimal_places=6)
     # latitude..
-    latitude = models.DecimalField(
-        verbose_name="Latitude", max_digits=9, decimal_places=6
-    )
+    latitude = models.DecimalField(verbose_name="Latitude", max_digits=9, decimal_places=6)
     # website..
-    url = models.URLField(
-        verbose_name="Site Web", max_length=200, null=True, blank=True
-    )
+    url = models.URLField(verbose_name="Site Web", max_length=200, null=True, blank=True)
     auteur = models.CharField(max_length=125, verbose_name="Auteur")
     miseajour = models.DateField("Mise à jour", auto_now=True)
-    municipalite = models.ForeignKey(
-        Municipalite, on_delete=models.RESTRICT, verbose_name="Municipalité"
-    )
-    stakeholder = models.CharField(
-        verbose_name="Partenaire/propriétaire", max_length=255, null=True, blank=True
-    )
+    municipalite = models.ForeignKey(Municipalite, on_delete=models.RESTRICT, verbose_name="Municipalité")
+    stakeholder = models.CharField(verbose_name="Partenaire/propriétaire", max_length=255, null=True, blank=True)
     periode_interet_debut = models.IntegerField(choices=Mois.choices)
-    periode_interet_fin = models.IntegerField(
-        choices=Mois.choices, null=True, blank=True
-    )
-    sous_habitat = models.ManyToManyField(
-        SousHabitat, related_name="sous_habitats", verbose_name=("Sous habitat")
-    )
-    description_generale = models.TextField(
-        verbose_name="Description générale", max_length=1024, blank=True, null=True
-    )
+    periode_interet_fin = models.IntegerField(choices=Mois.choices, null=True, blank=True)
+    sous_habitat = models.ManyToManyField(SousHabitat, related_name="sous_habitats", verbose_name=("Sous habitat"))
+    description_generale = models.TextField(verbose_name="Description générale", max_length=1024, blank=True, null=True)
     transport = models.BooleanField(verbose_name="Transport en commun", default=False)
     acces_gratuit = models.BooleanField(verbose_name="Accès gratuit", default=False)
     toilette = models.BooleanField(verbose_name="Toilettes", default=False)
     picnique = models.BooleanField(verbose_name="Table à picnique", default=False)
     banc = models.BooleanField(verbose_name="Banc disponible", default=False)
 
-    bon_a_savoir = models.TextField(
-        verbose_name="Bon à savoir", max_length=1024, blank=True, null=True
-    )
-    enjeux = models.TextField(
-        verbose_name="Enjeux de conservation", max_length=1024, blank=True, null=True
-    )
+    bon_a_savoir = models.TextField(verbose_name="Bon à savoir", max_length=1024, blank=True, null=True)
+    enjeux = models.TextField(verbose_name="Enjeux de conservation", max_length=1024, blank=True, null=True)
     status = models.IntegerField(choices=Status.choices, default=1)
 
     def __str__(self):
@@ -88,13 +66,9 @@ class SiteOrnitho(models.Model):
 
 
 class SiteOrnithoImage(models.Model):
-    siteornitho = models.ForeignKey(
-        SiteOrnitho, on_delete=models.CASCADE, related_name="images"
-    )
+    siteornitho = models.ForeignKey(SiteOrnitho, on_delete=models.CASCADE, related_name="images")
     title = models.CharField(verbose_name="Titre", max_length=125)
-    description = models.CharField(
-        verbose_name="Description", max_length=55, null=True, blank=True
-    )
+    description = models.CharField(verbose_name="Description", max_length=55, null=True, blank=True)
     photo_author = models.CharField(verbose_name="Auteur", max_length=125)
     photo_date = models.DateField(verbose_name="Date photo")
     image = models.ImageField(upload_to="siteornitho/images")
