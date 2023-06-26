4bd5520e005cb1bfb65a215915c0497558c40105
Mario St-Gelais <mario.stg@videotron.ca>
2023-06-26 15:46:41 -0400

    set some formatting and adjust layout

--- main/settings_dev.py
+++ main/settings_dev.py
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 LANGUAGE_CODE = "fr-ca"
-LANGUAGE_CODE = "en-us"
+# LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "EST"
 
